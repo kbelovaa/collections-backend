4010b22608d6a646dddf75abfb1c00c27324b0bb
Ekaterina Belova <kbelovaa17@gmail.com>
2023-07-21 13:57:24 +0300

    Changes to create an admin

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/all', checkRoleMiddleware('ADMIN'), userController.getAll);
 router.get('/:id', userController.getOne);
 router.patch('/status', checkRoleMiddleware('ADMIN'), userController.changeStatus);
-router.patch('/rights', checkRoleMiddleware('ADMIN'), userController.assignRights);
+router.patch('/rights', authMiddleware, userController.assignRights);
 router.delete('/delete/:id', checkRoleMiddleware('ADMIN'), userController.delete);
 
 module.exports = router;
